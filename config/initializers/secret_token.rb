0fa20c92a701a5f5f09fcfd45feb10852a6ef5b2
Jennifer Shehane <shehane.jennifer@gmail.com>
2013-11-16 13:04:52 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Craigslator::Application.config.secret_token = 'b6b7057ad36835a4e6df2e982f3c60f54e9155b535ab477a817a55d35a2a13de44760c4f0dcef70ba1aae6635535c3044d854ed97ed51c88c165874921bed3c8'
